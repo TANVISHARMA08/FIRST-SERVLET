a08ac8a81c47053cb0836e645987d76234e98581
TANVISHARMA08 <tanvi8sh@gmail.com>
2021-06-15 20:58:56 +0530

    UC1

---
+++ src/main/java/com/bl/Firstservlet/Sampleservlet.java
@@ -0,0 +1,30 @@
+package com.bl.Firstservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SampleServlet")
+public class Sampleservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Sampleservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h3> Hello Servlet </h3>");
+		out.close();
+	}
+
+	
+}
